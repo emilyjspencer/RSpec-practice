12e792572e5e7744f7a691fa7905fab9d710818d
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-15 14:15:34 +0000

    Double responds to two methods

--- stuntdouble/spec/stunt_double_spec.rb
+++ stuntdouble/spec/stunt_double_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe 'a random double' do
   it 'only allows defined methods to be invoked' do
-    stunt_double = double("Angelina Jolie", fly_through_air: "Whooosh") # create a double of Angelina Jolie called stunt_double
+    stunt_double = double("Angelina Jolie", fly_through_air: "Whooosh", scale_building: "Awesome") # create a double of Angelina Jolie called stunt_double
     stunt_double.fly_through_air # and allow the double to respond to the method and return "Whoooosh"
-  end  
+    stunt_double.scale_building 
+  end  # and the method scale_building 
 end 
 
 #=>
