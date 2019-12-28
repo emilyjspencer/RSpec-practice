16a0849b53f3ee61ef4b358ca6bcf283d8d175ba
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-28 16:15:21 +0000

    Adds third test

--- allow/spec/allow_spec.rb
+++ allow/spec/allow_spec.rb
@@ -7,7 +7,14 @@ RSpec.describe 'stubbing methods' do
 
   it 'can stub the length method' do
     array = [4, 5, 6]
-    allow(array).to receive(:length).and_return 20 # in reality this should be 3
+    allow(array).to receive(:length).and_return 20 # in reality this should be 3 
     expect(array.length).to eq 20
   end 
+
+  it 'adds an element to the end of the array' do
+    array = [4, 5, 6]
+    array.push(7)
+    expect(array).to eq [4, 5, 6, 7]  # we don't falsify this method 
+  end 
+
 end 
